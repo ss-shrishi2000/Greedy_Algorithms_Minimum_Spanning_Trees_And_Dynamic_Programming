43b4a21effc00e29f821cc8d719e7441c80da380
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2021-09-16 00:52:02 +0530

    Create LRU_Caching_Problem_Implementation.cpp

---
+++ LRU_Caching_Problem_Implementation.cpp
@@ -0,0 +1,89 @@
+
+Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+Implement the LRUCache class:
+LRUCache(int capacity) - Initialize the LRU cache with positive size capacity.
+int get(int key) - Return the value of the key if the key exists, otherwise return -1.
+void put(int key, int value) - Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. 
+If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+The functions get and put must each run in O(1) average time complexity.
+
+
+Example 1:
+Input
+["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+Output :- [null, null, null, 1, null, -1, null, -1, 3, 4]
+
+Explanation
+LRUCache lRUCache = new LRUCache(2);
+lRUCache.put(1, 1); // cache is {1=1}
+lRUCache.put(2, 2); // cache is {1=1, 2=2}
+lRUCache.get(1);    // return 1
+lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+lRUCache.get(2);    // returns -1 (not found)
+lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+lRUCache.get(1);    // return -1 (not found)
+lRUCache.get(3);    // return 3
+lRUCache.get(4);    // return 4
+
+SOLUTION :-
+    
+
+using key = int;
+
+struct Entry 
+{
+    int value;
+    list<key>::iterator itr;
+};
+
+class LRUCache 
+{
+    unordered_map<key, Entry> _cache;
+    //old keys move to back, new ones enter at front
+    list<key> _age;
+    int _capacity;
+public:
+    LRUCache(int capacity) {
+        _capacity = capacity;
+    }
+
+    int get(int key) 
+    {
+        //if key is present, renew its age and return value
+        if (_cache.count(key)) {
+            _age.erase(_cache[key].itr);
+            _age.push_front(key);
+            _cache[key].itr = _age.begin();
+            return _cache[key].value;
+        }
+        return -1;
+    }
+
+    void put(int key, int value) 
+    {
+        //if key is not present, check if the old entry need to be erased
+        if (!_cache.count(key)) 
+        {
+            if (_cache.size() == _capacity) 
+            {
+                _cache.erase(_age.back());
+                _age.pop_back();
+            }
+        }
+        //key is present, just renew the age
+        else 
+        {
+            _age.erase(_cache[key].itr);
+        }
+        _age.push_front(key);
+        _cache[key] = { value, _age.begin() };
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
