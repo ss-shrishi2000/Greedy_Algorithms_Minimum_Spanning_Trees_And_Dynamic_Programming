9f009e291ed4db648fab2e4c864981cf733151fb
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2021-10-08 00:07:20 +0530

    Update Spanning_Tree_With_Maximum_Degree.cpp

--- Spanning_Tree_With_Maximum_Degree.cpp
+++ Spanning_Tree_With_Maximum_Degree.cpp
@@ -1,3 +1,48 @@
+You are given an undirected unweighted connected graph consisting of n vertices and m edges. It is guaranteed that there are no self-loops or multiple edges in 
+the given graph. Your task is to find any spanning tree of this graph such that the maximum degree over all vertices is maximum possible. Recall that the degree 
+of a vertex is the number of edges incident to it.
+
+Input :-
+The first line contains two integers n and m (2≤n≤2⋅105, n−1≤m≤min(2⋅105,n(n−1)2)) — the number of vertices and edges, respectively.
+The following m lines denote edges: edge i is represented by a pair of integers vi, ui (1≤vi,ui≤n, ui≠vi), which are the indices of vertices connected by 
+the edge. There are no loops or multiple edges in the given graph, i. e. for each pair (vi,ui) there are no other pairs (vi,ui) or (ui,vi) in the list of edges, 
+and for each pair (vi,ui) the condition vi≠ui is satisfied.
+
+Output :-
+Print n−1 lines describing the edges of a spanning tree such that the maximum degree over all vertices is maximum possible. Make sure that the edges of the printed 
+spanning tree form some subset of the input edges (order doesn't matter and edge (v,u) is considered the same as the edge (u,v)).
+If there are multiple possible answers, print any of them.
+   
+	Examples :-
+						   
+inputCopy
+5 5
+1 2
+2 3
+3 5
+4 3
+1 5
+outputCopy
+3 5
+2 1
+3 2
+3 4
+inputCopy
+4 6
+1 2
+1 3
+1 4
+2 3
+2 4
+3 4
+outputCopy
+4 1
+1 2
+1 3
+						   
+SOLUTION :-
+												 
+
 #include<bits/stdc++.h>
 
 using namespace std;
